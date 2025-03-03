7eb3cf86e1c7ab9a76878bbf1d81530e43b8687c
lamvietdat <166464678+lamvietdat@users.noreply.github.com>
2025-03-03 21:56:36 +0700

    lesson04

---
+++ com.example.devmaster.lesson04/com.example.devmaster.lesson04/src/main/java/com/example/devmaster/lesson04/lesson04Application.java
@@ -0,0 +1,11 @@
+package com.example.devmaster.lesson04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class lesson04Application {
+    public static void main(String[] args) {
+        SpringApplication.run(lesson04Application.class,args);
+    }
+}
